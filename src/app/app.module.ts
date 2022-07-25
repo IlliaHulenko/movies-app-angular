e9c499610264f088059cf0e3e17cea3fa22a022a
Illia Hulenko <exmentos2017@yahoo.com>
2022-07-26 00:15:29 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MovieTileComponent } from './components/movie-tile/movie-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    MoviesHomeComponent,
+    AddMoviesComponent,
+    ButtonComponent,
+    MovieTileComponent
   ],
   imports: [
     BrowserModule,
